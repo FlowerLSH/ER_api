b95a13e7f321ed0df290f44bf14535063491e023
cmhclient <cmh238840@naver.com>
2024-05-19 16:58:13 +0900

    Create 함수모음집.py

---
+++ 함수모음집.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+# 유저 닉네임으로 id번호 받기
+def GetNickNum():
+    nick = input("유저 닉네임 입력 : ")
+    url = "https://open-api.bser.io/v1/user/nickname?query=%s"%(nick)
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'user' in data:
+            user_id = data['user']['userNum']
+            print(user_id)
+        else:
+            print("해당 닉네임의 유저를 찾을 수 없습니다.")
+    else:
+        print("API 요청 실패:", response.status_code)
+        print(response.text)
+
+#유저 id로 닉네임, mmr, 랭킹 출력
+def GetUserRank():
+    UserNum = int(input("유저 넘버 입력 : "))
+    season = int(input("시즌 입력 : "))
+    Team = int(input("모드 입력(1. 솔로 , 2. 듀오, 3. 스쿼드) : "))
+    
+    url = "https://open-api.bser.io/v1/rank/%d/%d/%d"%(UserNum, season, Team)
+
+    response = requests.get(url, headers = headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        if 'userRank' in data:
+            user_rank = data['userRank']
+            user = user_rank.get('nickname')
+            mmr = user_rank.get('mmr')
+            rank = user_rank.get('rank')
+            print("User:", user)
+            print("MMR:", mmr)
+            print("Rank:", rank)
+            
+        else:
+            print("해당 유저의 랭크 정보를 찾을 수 없습니다.")
+
+    else:
+        print("API 요청 실패:", response.status_code)
+        print(response.text)
