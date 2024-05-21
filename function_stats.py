b49d08a8e3f7ed040ec30d7efe79d984c0bf74d7
FlowerLSH <flowerlsh0118@gmail.com>
2024-05-21 13:56:02 +0900

    클래스화

--- function_stats.py
+++ function_stats.py
@@ -1,61 +1,62 @@
 import requests
 import json
 
-headers = {"x-api-key" : "여기에 api키 입력"}
+class function_stats:
+    def __init__(self, api_key):
+        self.api_key = {"x-api-key" : api_key}
 
-# 유저 닉네임으로 id번호 받기
-def GetNickNum(nick):
-    url = "https://open-api.bser.io/v1/user/nickname?query=%s"%(nick)
+    def GetNickNum(self, nick):
+        url = "https://open-api.bser.io/v1/user/nickname?query=%s"%(nick)
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=self.api_key)
 
-    if response.status_code == 200:
-        data = response.json()
-        if 'user' in data:
-            user_id = data['user']['userNum']
-            print(user_id)
+        if response.status_code == 200:
+            data = response.json()
+            if 'user' in data:
+                user_id = data['user']['userNum']
+                print(user_id)
+            else:
+                print("해당 닉네임의 유저를 찾을 수 없습니다.")
         else:
-            print("해당 닉네임의 유저를 찾을 수 없습니다.")
-    else:
-        print("API 요청 실패:", response.status_code)
-        print(response.text)
+            print("API 요청 실패:", response.status_code)
+            print(response.text)
 
-#유저 id로 닉네임, mmr, 랭킹 출력
-def GetUserRank(UserNum, season, Team):
-    
-    url = "https://open-api.bser.io/v1/rank/%d/%d/%d"%(UserNum, season, Team)
-
-    response = requests.get(url, headers = headers)
-
-    if response.status_code == 200:
-        data = response.json()
+    #유저 id로 닉네임, mmr, 랭킹 출력
+    def GetUserRank(self, UserNum, season, Team):
         
-        if 'userRank' in data:
-            user_rank = data['userRank']
-            user = user_rank.get('nickname')
-            mmr = user_rank.get('mmr')
-            rank = user_rank.get('rank')
-            print("User:", user)
-            print("MMR:", mmr)
-            print("Rank:", rank)
-            
-        else:
-            print("해당 유저의 랭크 정보를 찾을 수 없습니다.")
+        url = "https://open-api.bser.io/v1/rank/%d/%d/%d"%(UserNum, season, Team)
 
-    else:
-        print("API 요청 실패:", response.status_code)
+        response = requests.get(url, headers = self.api_key)
+
+        if response.status_code == 200:
+            data = response.json()
+            
+            if 'userRank' in data:
+                user_rank = data['userRank']
+                user = user_rank.get('nickname')
+                mmr = user_rank.get('mmr')
+                rank = user_rank.get('rank')
+                print("User:", user)
+                print("MMR:", mmr)
+                print("Rank:", rank)
+                
+            else:
+                print("해당 유저의 랭크 정보를 찾을 수 없습니다.")
+
+        else:
+            print("API 요청 실패:", response.status_code)
+            print(response.text)
+
+    #유저 통계 획득(통계가 매우 기므로 주의)
+    def GetUserStats(self, userNum):
+        url = "https://open-api.bser.io/v1/user/games/%d"%(userNum)
+        
+        response = requests.get(url, headers = self.api_key)
         print(response.text)
 
-#유저 통계 획득(통계가 매우 기므로 주의)
-def GetUserStats(userNum):
-    url = "https://open-api.bser.io/v1/user/games/%d"%(userNum)
-    
-    response = requests.get(url, headers = headers)
-    print(response.text)
+    #시즌 번호, 매칭 모드(솔로,듀오, 스쿼드) 입력하고 탑 1000 확인
+    def GetTopRankers(self, season, team):
+        url = "https://open-api.bser.io/v1/rank/top/%d/%d"%(season, team)
 
-#시즌 번호, 매칭 모드(솔로,듀오, 스쿼드) 입력하고 탑 1000 확인
-def GetTopRankers(season, team):
-    url = "https://open-api.bser.io/v1/rank/top/%d/%d"%(season, team)
-
-    response = requests.get(url, headers= headers)
-    print(response.text)
+        response = requests.get(url, headers= self.api_key)
+        print(response.text)
---
+++ main.py
@@ -0,0 +1,5 @@
+import function_matchs as fm
+import function_stats as fs
+
+nick = "하얀수수깡"
+fs.GetNickNum(nick)
